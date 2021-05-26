d9a25c3187c3f2e9f895b8af9b1607ce3847cea1
Umbertobf17 <umbertobf17@gmail.com>
2021-05-26 15:13:26 +0200

    Sistemati User e Admin Dash

--- src/HollowMindsDev.BackEnd.WebApp/Pages/Shared/adminDash.cshtml.cs
+++ src/HollowMindsDev.BackEnd.WebApp/Pages/Shared/adminDash.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HollowMindsDev.BackEnd.ApplicationCore.Entities.Silos;
+using HollowMindsDev.BackEnd.Services.Interfaces.ISilos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,18 @@ namespace HollowMindsDev.BackEnd.WebApp.Pages.Shared
 {
     public class adminDashModel : PageModel
     {
+        private readonly IMeasurementService _measurementService;
+
+        public adminDashModel(IMeasurementService measurementService)
+        {
+            _measurementService = measurementService;
+        }
+
+        public List<Measurement> Last { get; set; }
+
         public void OnGet()
         {
+            Last = (List<Measurement>)_measurementService.GetLastMeasurement();
         }
     }
 }
