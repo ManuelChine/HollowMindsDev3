9dac5e7b951d28aad8cadaebf3fbe46e67fa76a2
andrea01-trevi <andrea.trevisan@stud.tecnicosuperiorekennedy.it>
2021-05-26 15:53:28 +0200

    Completate variabile pagine Dash 5-7

--- src/HollowMindsDev.BackEnd.WebApp/Pages/Shared/Dash5.cshtml.cs
+++ src/HollowMindsDev.BackEnd.WebApp/Pages/Shared/Dash5.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HollowMindsDev.BackEnd.Services.Interfaces.ViewModel;
+using HollowMindsDev.BackEnd.Services.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,19 @@ namespace HollowMindsDev.BackEnd.WebApp.Pages.Shared.DashBoardSilos
 {
     public class Dash5Model : PageModel
     {
+        private readonly IMeasurementModelService _measurementModelService;
+
+        public Dash5Model(IMeasurementModelService measurementModelService)
+        {
+            _measurementModelService = measurementModelService;
+        }
+
+        public List<MeasurementModel> Last { get; set; }
+
         public void OnGet()
         {
+            Last = _measurementModelService.GetAllModel();
         }
     }
 }
+
