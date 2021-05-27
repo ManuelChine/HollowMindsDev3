490d6ee0f276177887ba21affd3a6308cd22605c
Umbertobf17 <umbertobf17@gmail.com>
2021-05-27 16:23:16 +0200

    Aggiunta funzionalità e bottoni di level Settings

--- src/HollowMindsDev.BackEnd.WebApp/Pages/Shared/levelSettings.cshtml.cs
+++ src/HollowMindsDev.BackEnd.WebApp/Pages/Shared/levelSettings.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HollowMindsDev.BackEnd.ApplicationCore.Entities.Silos;
+using HollowMindsDev.BackEnd.Services.Interfaces.ISilos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,18 @@ namespace HollowMindsDev.BackEnd.WebApp.Pages.Shared
 {
     public class levelSettingsModel : PageModel
     {
-        public void OnGet()
+        private readonly ILimitService _limitService;
+
+        public levelSettingsModel(ILimitService limitService)
         {
+            _limitService = limitService;
+        }
+        public Limit Limit { get; set; }
+
+        public void OnPostSubmit(Limit limit)
+        {
+
+            _limitService.InsertLimit(limit);
         }
     }
 }
